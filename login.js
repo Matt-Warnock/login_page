49cd881f2d75575af1ebf7c522384df79c07842e
MattW <drflamerock@yahoo.co.uk>
2020-04-11 19:05:59 +0100

    Added salt to password and added verifiying function

--- login.js
+++ login.js
@@ -1,13 +1,23 @@
-const output = document.getElementById('validation');
-const button = document.getElementById('submit');
+function verifiyPassword(hash) {
+  checkpw("12345678", hash, displayResult, null);
+  return;
+}
 
-button.addEventListener('click', function() {
+function displayResult(result) {
+  const output = document.getElementById('validation');
+  
+  return result ? output.textContent = 'Welcome Back!' :
+  output.textContent = 'Invalid password';
+}
+
+
+document.getElementById('submit').addEventListener('click', function() {
+
   let password = document.getElementById('password_field').value;
+  let salt = gensalt(5);
 
-  if (password === '12345678') {
-    output.textContent = 'Welcome Back!';
-  }
-  else {
-    output.textContent = 'Invalid password';
-  }
+
+  hashpw(password, salt, verifiyPassword, null);
+
+
 });
