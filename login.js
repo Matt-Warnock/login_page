f8ce9064eb1cacc866d41d0d8d35f39a1376d9c6
MattW <drflamerock@yahoo.co.uk>
2020-04-14 18:52:47 +0100

    Improve arry position format for readability

--- login.js
+++ login.js
@@ -1,28 +1,31 @@
-const passwords = ['greenbear757', 'organicswan134', 'bluemouse519', 'heavytiger886', 'heavyladybug575'];
 let isAMatch;
 
 function verifiyPassword(hash) {
-  isAMatch = [];
-  passwords.forEach( (item) => checkpw(item, hash, (result) => {return isAMatch.push(result);}, null));
-}
+  const passwords = ['greenbear757', 'organicswan134', 'bluemouse519', 'heavytiger886', 'heavyladybug575'];
 
+  isAMatch = [];
+
+  passwords.forEach(
+    (item) => checkpw(item, hash,
+    (result) => {return isAMatch.push(result);}, null));
+}
 
 function displayResult() {
   const output = document.getElementById('validation');
 
-  return isAMatch.some(match => match === true) ? output.textContent = 'Welcome Back!' :
-  output.textContent = 'Invalid password';
+  return isAMatch.some(
+           (match) => match === true) ?
+            output.textContent = 'Welcome Back!' :
+            output.textContent = 'Invalid password';
 }
-
 
 
 document.getElementById('submit').addEventListener('click', function() {
 
-  let password = document.getElementById('password_field').value;
+  let userPassword = document.getElementById('password_field').value;
   let salt = gensalt(5);
 
-
-  hashpw(password, salt, verifiyPassword, null);
+  hashpw(userPassword, salt, verifiyPassword, null);
   setTimeout(displayResult, 500);
 
 });
