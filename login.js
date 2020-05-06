be9402d59889287012078407646dacd736ab6e15
MattW <drflamerock@yahoo.co.uk>
2020-05-06 14:02:24 +0100

    Fix arrow for firefox covert js to objects

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-let isAMatch;
+/*let isAMatch;
 
 function verifiyPassword(hash) {
   const passwords = ['greenbear757', 'organicswan134', 'bluemouse519', 'heavytiger886', 'heavyladybug575'];
@@ -6,8 +6,7 @@ function verifiyPassword(hash) {
   isAMatch = [];
 
   passwords.forEach(
-    (item) => checkpw(item, hash,
-    (result) => {return isAMatch.push(result);}, null));
+    (item) => checkpw(item, hash, (result) => {return isAMatch.push(result);}, null));
 }
 
 function displayResult() {
@@ -28,4 +27,37 @@ document.getElementById('submit').addEventListener('click', function() {
   hashpw(userPassword, salt, verifiyPassword, null);
   setTimeout(displayResult, 500);
 
-});
+});*/
+
+
+(function () {
+  let passwordChecker = {
+    _passwords: ['greenbear757', 'organicswan134', 'bluemouse519', 'heavytiger886', 'heavyladybug575'],
+    isAMatch: [],
+
+    verifiyPassword(hash) {
+      this._passwords.forEach((item) => checkpw(item, hash, (result) => {return this.isAMatch.push(result);}, null));
+    }
+  };
+
+const ui = {
+  userPassword: document.getElementById('password_field').value,
+  _output: document.getElementById('validation'),
+  _salt: gensalt(5),
+
+  displayResult() {
+    return passwordChecker.isAMatch.some((match) => match === true) ?
+           this._output.textContent = 'Welcome Back!' :
+           this._output.textContent = 'Invalid password';
+  },
+
+  clickStart() {
+    document.getElementById('submit').addEventListener('click', () => {
+
+      hashpw(this.userPassword, this._salt, passwordChecker.verifiyPassword, null);
+      setTimeout(this.displayResult, 500);
+
+    });
+  }
+};
+})();
