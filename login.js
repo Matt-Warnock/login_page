34f45d6de773c662ddd89149e56f1b517afc6461
MattW <drflamerock@yahoo.co.uk>
2020-05-06 16:29:53 +0100

    Add getters and explicit calls where needed

--- login.js
+++ login.js
@@ -1,63 +1,42 @@
-/*let isAMatch;
-
-function verifiyPassword(hash) {
-  const passwords = ['greenbear757', 'organicswan134', 'bluemouse519', 'heavytiger886', 'heavyladybug575'];
-
-  isAMatch = [];
-
-  passwords.forEach(
-    (item) => checkpw(item, hash, (result) => {return isAMatch.push(result);}, null));
-}
-
-function displayResult() {
-  const output = document.getElementById('validation');
-
-  return isAMatch.some(
-           (match) => match === true) ?
-            output.textContent = 'Welcome Back!' :
-            output.textContent = 'Invalid password';
-}
-
-
-document.getElementById('submit').addEventListener('click', function() {
-
-  let userPassword = document.getElementById('password_field').value;
-  let salt = gensalt(5);
-
-  hashpw(userPassword, salt, verifiyPassword, null);
-  setTimeout(displayResult, 500);
-
-});*/
-
-
 (function () {
   let passwordChecker = {
     _passwords: ['greenbear757', 'organicswan134', 'bluemouse519', 'heavytiger886', 'heavyladybug575'],
-    isAMatch: [],
+    _isAMatch: [],
+
+    get isAMatch() {
+      return this._isAMatch;
+    },
 
     verifiyPassword(hash) {
-      this._passwords.forEach((item) => checkpw(item, hash, (result) => {return this.isAMatch.push(result);}, null));
+      passwordChecker._isAMatch = [];
+      let storeResults = (result) => {return passwordChecker._isAMatch.push(result);};
+      passwordChecker._passwords.forEach((item) => checkpw(item, hash,storeResults , null));
     }
   };
 
-const ui = {
-  userPassword: document.getElementById('password_field').value,
-  _output: document.getElementById('validation'),
-  _salt: gensalt(5),
+  const ui = {
+    _userPassword: document.getElementById('password_field'),
+    _output: document.getElementById('validation'),
+    _salt: gensalt(5),
 
-  displayResult() {
-    return passwordChecker.isAMatch.some((match) => match === true) ?
-           this._output.textContent = 'Welcome Back!' :
-           this._output.textContent = 'Invalid password';
-  },
+    get userPassword() {
+      return this._userPassword.value;
+    },
 
-  clickStart() {
-    document.getElementById('submit').addEventListener('click', () => {
+    displayResult() {
+      return passwordChecker.isAMatch.some((match) => match) ?
+      ui._output.textContent = 'Welcome Back!' :
+      ui._output.textContent = 'Invalid password';
+    },
 
-      hashpw(this.userPassword, this._salt, passwordChecker.verifiyPassword, null);
-      setTimeout(this.displayResult, 500);
+    clickStart() {
+      document.getElementById('submit').addEventListener('click', () => {
+        hashpw(this.userPassword, this._salt, passwordChecker.verifiyPassword, null);
+        setTimeout(this.displayResult, 500);
+      });
+    }
+  };
 
-    });
-  }
-};
+  ui.clickStart();
+
 })();
