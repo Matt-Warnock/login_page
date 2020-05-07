0381582c52669f47ded48701fda596a4e9e7db8b
MattW <drflamerock@yahoo.co.uk>
2020-05-07 14:21:38 +0100

    Refactor to sperate concerns change getter and and setter

--- login.js
+++ login.js
@@ -1,38 +1,48 @@
 (function () {
   let passwordChecker = {
     _passwords: ['greenbear757', 'organicswan134', 'bluemouse519', 'heavytiger886', 'heavyladybug575'],
-    _isAMatch: [],
+    isAMatch: [],
 
-    get isAMatch() {
-      return this._isAMatch;
+    get passwords() {
+      return passwordChecker._passwords;
     },
 
     verifiyPassword(hash) {
-      passwordChecker._isAMatch = [];
-      let storeResults = (result) => {return passwordChecker._isAMatch.push(result);};
-      passwordChecker._passwords.forEach((item) => checkpw(item, hash,storeResults , null));
-    }
+      passwordChecker.isAMatch = [];
+      let storeResults = result => passwordChecker.isAMatch.push(result);
+      passwordChecker.passwords.forEach(password => checkpw(password, hash, storeResults, null));
+    },
+
+    userHash(enteredPassword) {
+      const salt = gensalt(5);
+      hashpw(enteredPassword, salt, passwordChecker.verifiyPassword, null);
+    },
+
+    checkResult() {
+      let passwordMatched = this.isAMatch.some(match => match);
+      ui.displayResult = passwordMatched;
+    },
   };
 
   const ui = {
     _userPassword: document.getElementById('password_field'),
     _output: document.getElementById('validation'),
-    _salt: gensalt(5),
 
     get userPassword() {
       return this._userPassword.value;
     },
 
-    displayResult() {
-      return passwordChecker.isAMatch.some((match) => match) ?
-      ui._output.textContent = 'Welcome Back!' :
-      ui._output.textContent = 'Invalid password';
+    set displayResult(match) {
+      if(typeof match === 'boolean')
+      match ? ui._output.textContent = 'Welcome Back!' :
+              ui._output.textContent = 'Invalid password';
     },
 
     clickStart() {
       document.getElementById('submit').addEventListener('click', () => {
-        hashpw(this.userPassword, this._salt, passwordChecker.verifiyPassword, null);
-        setTimeout(this.displayResult, 500);
+        passwordChecker.userHash(this.userPassword);
+        setTimeout(() => passwordChecker.checkResult(), 500);
+
       });
     }
   };
